b2ee74152aa2e50dfafbe44ae83238d900fde97f
marcoalonso <marcoalonsorota@hotmail.com>
2021-07-26 19:42:21 -0500

    Empezando

---
+++ OrderEats/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  OrderEats
+//
+//  Created by marco rodriguez on 26/07/21.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
